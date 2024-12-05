b28f94bd18be06c9adf95dc56717ce4d44ce6d51
Noah LOUIS <93314664+Kalipse@users.noreply.github.com>
2024-12-05 13:27:00 +0100

    Update cosmos.js

--- cosmos.js
+++ cosmos.js
@@ -1,27 +1,31 @@
+// cosmos.js
 const { MongoClient } = require('mongodb');
 
 // URI de connexion à Cosmos DB avec l'API MongoDB
 const uri = "mongodb://linkuplnb-server:hZd1Gb0lCV1shCeNEMgFUbWLcCrWcsLprtZGBiS6kXELaJcp31jos3ezFammTIK1bZOYcRLdqnLmACDbzHkpGg%3D%3D@linkuplnb-server.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@linkuplnb-server@";
 
-// Connexion à Cosmos DB via MongoDB
-MongoClient.connect(uri, { useUnifiedTopology: true }, (err, client) => {
-  if (err) {
+// Client MongoDB
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+// Connexion à Cosmos DB
+const connectToDb = async () => {
+  try {
+    await client.connect();
+    console.log("Connecté à Cosmos DB via MongoDB");
+  } catch (err) {
     console.error("Erreur de connexion à Cosmos DB via MongoDB:", err);
-    return;
   }
-
-  // Connexion réussie
-  console.log("Connecté à Cosmos DB via MongoDB");
-
-  // Identifiants de la base de données et de la collection
-  const databaseId = 'linkup-database';
-  const containerId = 'LinkUp';
-
-  const db = client.db(databaseId); // Utilisation de MongoClient pour obtenir la base de données
-  const collection = db.collection(containerId); // Utilisation de MongoClient pour obtenir la collection
-
-module.exports = {
-  getContainer: () => {
-    return client.db(databaseId).collection(containerId);
-  },
 };
+
+// Connexion à la base de données et la collection
+const databaseId = 'linkup-database';
+const containerId = 'LinkUp';
+
+const getContainer = () => {
+  return client.db(databaseId).collection(containerId);
+};
+
+// Appel de la fonction de connexion
+connectToDb();
+
+module.exports = { getContainer };
